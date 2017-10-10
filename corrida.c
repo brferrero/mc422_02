8b33d9c66aadaafe13cc712c37de4c9d99e86552
brferrero <brferrero@gmail.com>
2017-10-10 20:30:31 -0300

    criando as threads

--- corrida.c
+++ corrida.c
@@ -13,12 +13,10 @@
 #define MAX_STRING 32
 #define EPSILON 0.0000000001
 
-int cpu = 0;
-char processo_atual[MAX_STRING];
-int *PISTA;
+/*pista de largura 10*/
+int *PISTA[10];
 
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
-pthread_t thread1;
 
 /*gera um numero aleatorio entre 0 e 99*/
 int lottery (int clock);
@@ -26,12 +24,16 @@ int lottery (int clock);
  e devolve 1 se o sorteio teve sucesso com a chance dada*/
 int speed_lottery (int clock, int chance);
 
+void *ciclista (void *arg);
+
 /* www.ime.usp.br/~pf */
 void *mallocX (unsigned int nbytes);
 /*---------------------------------------------------------------------------*/
 
 int main(int argc, char *argv[])
 {
+    pthread_t *threads;
+    int i;
     /*relógio arbitrário: milisegundo*/
     int clock = 0;
 
@@ -45,25 +47,44 @@ int main(int argc, char *argv[])
     int v = 80;
     /* velocidade */
     int speed = 30;
-    int chance = 1;
-
-    PISTA = mallocX ((d)*sizeof(int));
+    int chance = 30;
+    
+    /*PISTA de comprimento d e largura 10*/
+    for (i = 0; i < 10; i++)
+         PISTA[i] = mallocX((d) * sizeof(int));
 
     /* argumentos */
-    if (argc  < 4) {
-        printf ("\n");
+    if (argc  == 4) {
+        d = atoi (argv[1]);
+        n = atoi (argv[2]);
+        v = atoi (argv[3]);
+        printf ("Pista tamanho: %d | n ciclistas: %d| v voltas: %d \n", d, n, v);
         /*exit(1);*/
     }
     else {
         if (argc == 5) printf ("debug\n");
+        else {
+            fprintf (stderr,"Erro de entrada!\n");
+            exit (EXIT_FAILURE);
+        }
     }
 
     /*condicoes da corrida*/
     if ( (d <= 249) || (n <= 5) || (n > 5*d) || (v%20 != 0)) {
         fprintf (stderr,"Erro nas condicoes de entrada\n");
-        exit(1);
+        exit (EXIT_FAILURE);
     }
-
+    
+    /*criar vetor de n threads*/
+    /*pthread_t  thread_ciclistas[n];*/
+    threads = mallocX (n * sizeof(pthread_t));
+    /*dispara os ciclistas (threads)*/
+    for (i = 0; i < n; i++) {
+        if ( pthread_create( &threads[i], NULL, ciclista, NULL) ) {
+            printf("Erro ao criar thread.");
+            abort ();
+        }
+    }
 
     /* SIMULADOR */
     while (1) {
@@ -83,20 +104,13 @@ int main(int argc, char *argv[])
 
 /*---------------------------------------------------------------------------*/
 
-void* processo (void* slep)
-{
-    double sleep = *((double*) slep);
-    pthread_mutex_lock ( &mutex1 );
-    cpu = 1;
-    pthread_mutex_unlock ( &mutex1 );
-
-    sleep = (int)sleep*1000000;
-    usleep (sleep);
-
-    pthread_mutex_lock( &mutex1 );
-    cpu = 0;
-    pthread_mutex_unlock( &mutex1 );
-    return NULL;
+void *ciclista(void *arg) {
+  int id;
+  id = 0;
+  pthread_mutex_lock ( &mutex1 );
+  PISTA[0][1] = id;
+  pthread_mutex_unlock ( &mutex1 );
+  return NULL;
 }
 
 /*gera aleatorios entre 0 e 99*/
